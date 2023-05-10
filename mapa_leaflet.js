608cbfdffcbad2cbbf7a5514fc5575aaeb79108e
SAPDEVEL.SAP CODE DEVELOPERS FOR GIT.900009 <sapdevel@cmima.csic.es>
2023-05-10 15:02:46 +0200

    AISmapV2

--- mapa_leaflet.js
+++ mapa_leaflet.js
@@ -1,6 +1,9 @@
-var map = L.map('map').setView([41.39742, 2.16328], 13);
+var map = L.map('map').setView([41.39742, 2.16328], 9);
+
+var AIS = L.geoJSON(staticAIS).addTo(map);
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap'
 }).addTo(map);
+
