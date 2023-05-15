e7cd0d349fad414342384ab9500fb283a91cce1f
JSalaCoro <jsalacor@gmail.com>
2023-05-15 11:58:00 +0200

    prova commit windows

--- mapa_leaflet.js
+++ mapa_leaflet.js
@@ -2,6 +2,7 @@ var map = L.map('map').setView([41.39742, 2.16328], 9);
 
 var AIS = L.geoJSON(staticAIS).addTo(map);
 
+//hola
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap'
